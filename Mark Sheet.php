7f558d1ca4b851d8cfd8fa90217e551a85c114e8
Kashafnek <kashafnek17@gmail.com>
2023-10-03 23:25:53 +0500

    PHP Basic Programs

---
+++ Mark Sheet.php
@@ -0,0 +1,20 @@
+<?php
+$marks = 75; 
+
+if ($marks >= 90) {
+    $grade = 'A+';
+} elseif ($marks >= 80) {
+    $grade = 'A';
+} elseif ($marks >= 70) {
+    $grade = 'B';
+} elseif ($marks >= 60) {
+    $grade = 'C';
+} elseif ($marks >= 50) {
+    $grade = 'D';
+} else {
+    $grade = 'F';
+}
+
+echo "Marks: $marks<br>";
+echo "Grade: $grade";
+?>
